b6359833234615726dbd9c8ef2a23ff66e09561e
Aadii7266 <adityaparihar7266@gmail.com>
2024-07-31 11:36:50 +0530

    Add files via upload

---
+++ BITWISE OPERATOR.cpp
@@ -0,0 +1,20 @@
+// aaditya parihar 23070123001
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"enter the value of a";
+    cin>> a;
+    cout<<"enter the value of b";
+    cin>>b;
+    cout<<"the value of bitwise opertator AND is "<<(a&b)<<endl;
+    cout<<"the value of bitwise opertator OR is "<<(a|b)<<endl;
+    cout<<"the value of bitwise opertator XOR is "<<(a^b)<<endl;
+    cout<<"the value of bitwise opertator NOT is "<<(~a)<<endl;
+    cout<<"the value of the bitwise operator left shift is "<<(a<<(2))<<endl;
+    cout<<"the value of the bitwise operator left shift is "<<(b<<(1))<<endl;
+    cout<<"the value of the bitwise operator  right shift is "<<(a>>(1))<<endl;
+    cout<<"the value of the bitwise operator  right shift is "<<(b<<(2))<<endl;
+
+}
